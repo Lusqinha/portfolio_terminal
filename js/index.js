5a06d7ffc5bc978e2ac9066ce070e279b7a9d5cf
Lucas Borges <lucas18sborges@gmail.com>
2024-02-26 16:34:45 -0300

    Adição da funcionalidade de navegação no histórico

--- js/index.js
+++ js/index.js
@@ -1,4 +1,10 @@
-import { app, delay, get_input_command, load_terminal, next_line } from './terminal.js';
+import {
+    app, delay,
+    get_input_command, get_recent_input,
+    navigate_on_history, load_terminal,
+    next_line
+} from './terminal.js';
+
 
 app.addEventListener("keypress", async function (e) {
     if (e.key === 'Enter') {
@@ -7,7 +13,7 @@ app.addEventListener("keypress", async function (e) {
     }
 })
 
-// ctrl + l para limpar o terminal
+
 app.addEventListener("keydown", async function (e) {
     if (e.ctrlKey && e.key === 'l') {
         e.preventDefault();
@@ -17,8 +23,20 @@ app.addEventListener("keydown", async function (e) {
 })
 
 app.addEventListener("click", async function (e) {
-    const input = document.querySelector('input');
+    const input = get_recent_input(); 
     input.focus();
+})
+
+
+app.addEventListener("keydown", async function (e) { 
+
+    if (e.key === 'ArrowUp') {
+        navigate_on_history('forward');
+    }
+    if (e.key === 'ArrowDown') {
+        navigate_on_history('backward');
+    }
+
 })
 
 
